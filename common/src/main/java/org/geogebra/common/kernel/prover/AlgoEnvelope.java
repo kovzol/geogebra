8ff528d3b4115fc1f2c356673a855ca7765dac03
Zoltán Kovács <zoltan@geogebra.org>
2020-11-07 14:49:12 +0100

    Version bump, revert silentmode change for Envelope

--- common/src/main/java/org/geogebra/common/kernel/prover/AlgoEnvelope.java
+++ common/src/main/java/org/geogebra/common/kernel/prover/AlgoEnvelope.java
@@ -223,9 +223,6 @@ public class AlgoEnvelope extends AlgoElement implements UsesCAS {
 	 * 
 	 */
 	public void computeEnvelope() {
-		if (kernel.isSilentMode()) {
-			return;
-		}
 		double startTime = UtilFactory.getPrototype().getMillisecondTime();
 		String result = null;
 		try {
