4256d62d7aafba9a4c682633687c4d495dbb7084
Zoltán Kovács <zoltan@geogebra.org>
2022-04-11 12:06:40 +0200

    Version bump

--- common/src/main/java/org/geogebra/common/GeoGebraConstants.java
+++ common/src/main/java/org/geogebra/common/GeoGebraConstants.java
@@ -14,7 +14,7 @@ public interface GeoGebraConstants {
 	/** complete version string */
 	public static final String VERSION_STRING = "5.0.641.0";
 	/** GeoGebra Discovery version string */
-	public static final String DISCOVERY_VERSION = "2022Mar16";
+	public static final String DISCOVERY_VERSION = "2022Apr11";
 
 	/** proper noun, should NOT be translated / transliterated */
 	public static final String APPLICATION_NAME = "GeoGebra";
