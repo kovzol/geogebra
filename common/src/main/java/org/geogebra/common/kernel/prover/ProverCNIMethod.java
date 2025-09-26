705072cc890710072187c830e655735f1788cbcf
Zoltán Kovács <zoltan@geogebra.org>
2025-09-26 14:48:04 +0200

    Giac code properly created for Example 7 (from arXiv paper)

--- common/src/main/java/org/geogebra/common/kernel/prover/ProverCNIMethod.java
+++ common/src/main/java/org/geogebra/common/kernel/prover/ProverCNIMethod.java
@@ -2,22 +2,215 @@ package org.geogebra.common.kernel.prover;
 
 import java.util.TreeSet;
 
+import org.geogebra.common.kernel.algos.AlgoElement;
+import org.geogebra.common.kernel.algos.AlgoIntersectLines;
+import org.geogebra.common.kernel.algos.AlgoJoinPointsSegment;
+import org.geogebra.common.kernel.algos.AlgoMidpoint;
+import org.geogebra.common.kernel.algos.AlgoMidpointSegment;
+import org.geogebra.common.kernel.algos.AlgoPolygon;
 import org.geogebra.common.kernel.geos.GeoElement;
+import org.geogebra.common.kernel.geos.GeoLine;
+import org.geogebra.common.kernel.geos.GeoPoint;
 import org.geogebra.common.util.Prover;
 import org.geogebra.common.util.Prover.ProofResult;
 import org.geogebra.common.util.debug.Log;
 
 public class ProverCNIMethod {
 
+	public static class CNIDefinition {
+		String declaration;
+		String realRelation;
+		boolean declarative;
+		boolean ignore;
+	}
+
 	public static ProofResult prove(Prover prover) {
 
 		GeoElement statement = prover.getStatement();
 
+		String declarations = "";
+		String realRelations = "";
+		int realRelationsNo = 0;
+
+		String VARIABLE_R_STRING = "r_"; // This must be a kind of unique string.
+		String VARIABLE_I_STRING = "I_"; // This must be a kind of unique string.
+
+		// All predecessors:
 		TreeSet<GeoElement> allPredecessors = statement.getAllPredecessors();
 
-		Log.debug("CNI prover finished");
+		// Free points. We need them to eliminate the variables according to them.
+		TreeSet<GeoElement> freePoints = new TreeSet<>();
+		for (GeoElement ge : allPredecessors) {
+			if (ge.getParentAlgorithm() == null) {
+				freePoints.add(ge);
+			} else {
+				// We also collect declarative and real-relational definitions.
+				CNIDefinition def = getCNIHypothesisDefinition(ge);
+				if (def == null) {
+					Log.debug("The CNI method does not yet implement " + ge.getParentAlgorithm().toString()
+						+ " which is required for " + ge.getLabelSimple());
+					return ProofResult.UNKNOWN;
+				}
+				if (def.ignore) {
+					continue; // for
+				}
+				if (def.declaration != null) {
+					declarations += def.declaration + "\n";
+				}
+				if (def.realRelation != null) {
+					String[] CASrealRelations = def.realRelation.split("\n");
+					for (String CASrealRelation : CASrealRelations) {
+						realRelationsNo++;
+						realRelations += CASrealRelation + "=" + VARIABLE_R_STRING + realRelationsNo + ",";
+					}
+				}
+			}
+		}
+
+		// Adding the thesis. This is very similar to the code above:
+		CNIDefinition def = getCNIThesisDefinition(statement);
+		if (def == null) {
+			Log.debug("The CNI method does not yet implement " + statement.getParentAlgorithm().toString()
+					+ " which is required for " + statement.getLabelSimple());
+			return ProofResult.UNKNOWN;
+		}
+		if (def.declaration != null) {
+			declarations += def.declaration;
+		}
+		if (def.realRelation != null) {
+			realRelations += def.realRelation + "=" + VARIABLE_R_STRING;
+		}
+
+		String[] predefinitions = {"coll(A_,B_,C_):=(A_-B_)/(A_-C_)"};
+
+		// Putting the code together...
+		String program = "";
+		program = "[";
+		for (String predefinition : predefinitions) {
+			program += "[" + predefinition + "],";
+		}
+		String[] declarationsA = declarations.split("\n");
+		for (String declaration : declarationsA) {
+			program += "[" + declaration + "],";
+		}
+		program += "[" + VARIABLE_I_STRING + ":=eliminate([" + realRelations + "],";
+		String freePointsL = "";
+		for (GeoElement ge : freePoints) {
+			freePointsL += getUniqueLabel(ge) + ",";
+		}
+		freePointsL = removeTail(freePointsL, 1);
+		program += "[" + freePointsL + "])]";
+		// Temporary code:
+		int codeLengthLines = predefinitions.length + declarationsA.length + 1;
+		program += "][" + (codeLengthLines - 1) + "]";
+
+		Log.debug("CNI program:\n" + program);
+
 		return ProofResult.UNKNOWN;
 
+	}
+
+	/** Create the CNI definition for a GeoElement (for a hypothesis).
+	 * Compute the rhs of the declaration String, the lhs of the real relation String,
+	 * and return them to the caller. This method should cover all algos sooner or later.
+	 * Now it is just a prototype that implements the CNI method some frequently used algos.
+	 *
+	 * @param ge the input GeoElement
+	 * @return all required information for the CNI definition for the input
+	 */
+	static CNIDefinition getCNIHypothesisDefinition(GeoElement ge) {
+		CNIDefinition c = new CNIDefinition();
+		AlgoElement ae = ge.getParentAlgorithm();
+		String gel = getUniqueLabel(ge);
+		// Declarations:
+		if (ae instanceof AlgoMidpoint) {
+			AlgoMidpoint am = (AlgoMidpoint) ae;
+			GeoElement P = am.getP();
+			GeoElement Q = am.getQ();
+			String Pl = getUniqueLabel(P);
+			String Ql = getUniqueLabel(Q);
+			c.declaration = gel + ":=(" + Pl + "+" + Ql + ")/2";
+			c.declarative = true;
+			return c;
+		}
+		if (ae instanceof AlgoMidpointSegment) {
+			AlgoMidpointSegment ams = (AlgoMidpointSegment) ae;
+			GeoElement P = ams.getP();
+			GeoElement Q = ams.getQ();
+			String Pl = getUniqueLabel(P);
+			String Ql = getUniqueLabel(Q);
+			c.declaration = gel + ":=(" + Pl + "+" + Ql + ")/2";
+			c.declarative = true;
+			return c;
+		}
+		// Real relations:
+		if (ae instanceof AlgoIntersectLines) {
+			AlgoIntersectLines ail = (AlgoIntersectLines) ae;
+			GeoLine g = ail.getg();
+			GeoLine h = ail.geth();
+			GeoPoint gS = g.getStartPoint();
+			GeoPoint gE = g.getEndPoint();
+			GeoPoint hS = h.getStartPoint();
+			GeoPoint hE = h.getEndPoint();
+			String gSl = getUniqueLabel(gS);
+			String gEl = getUniqueLabel(gE);
+			String hSl = getUniqueLabel(hS);
+			String hEl = getUniqueLabel(hE);
+			c.realRelation = "coll(" + gSl + "," + gEl + "," + gel + ")\n" +
+					"coll(" + hSl + "," + hEl + "," + gel + ")";
+			c.declarative = false;
+			return c;
+		}
+		if (ae instanceof AlgoPolygon || ae instanceof AlgoJoinPointsSegment) {
+			c.ignore = true;
+			return c;
+		}
+		// Unimplemented, but it should be handled...
+		return null;
+	}
+
+	/** Create the CNI definition for a GeoElement (for a thesis).
+	 * Compute the rhs of the declaration String, the lhs of the real relation String,
+	 * and return them to the caller. This method should cover all algos sooner or later.
+	 * Now it is just a prototype that implements the CNI method some frequently used algos.
+	 *
+	 * @param ge the input GeoElement
+	 * @return all required information for the CNI definition for the input
+	 */
+	static CNIDefinition getCNIThesisDefinition(GeoElement ge) {
+		CNIDefinition c = new CNIDefinition();
+		AlgoElement ae = ge.getParentAlgorithm();
+		if (ae instanceof AlgoAreCollinear) {
+			AlgoAreCollinear aac = (AlgoAreCollinear) ae;
+			GeoElement[] input = aac.getInput();
+			GeoElement A = input[0];
+			GeoElement B = input[1];
+			GeoElement C = input[2];
+			String Al = getUniqueLabel(A);
+			String Bl = getUniqueLabel(B);
+			String Cl = getUniqueLabel(C);
+			c.realRelation = "coll(" + Al + "," + Bl + "," + Cl + ")";
+			c.declarative = false;
+			return c;
+		}
+		// Unimplemented, but it should be handled...
+		return null;
+	}
+
+	/**
+	 * Return a label that is unique and can be inserted in a Giac code.
+	 * @param ge the input GeoElement
+	 * @return the label as String
+	 */
+	static String getUniqueLabel(GeoElement ge) {
+		return ge.getLabelSimple();
+	}
+
+	static String removeTail(String input, int length) {
+		if (input.length() >= length) {
+			return input.substring(0, input.length() - length);
+		}
+		return input;
 	}
 
 }
